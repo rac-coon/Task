082460dd25dadac1521c0fc91dba18f4739a462f
Nikita Solomientsev <38670044+araneoAnicet@users.noreply.github.com>
2020-08-28 18:58:21 +0300

    Remove comment

--- Protection.h
+++ Protection.h
@@ -4,7 +4,6 @@
 
 class Protection{
 protected:
-    // EDIT: virtual functions have no override in child classes. There is no reason to put a virtual modifier here.
     virtual std::string string_protection();
     virtual int int_protection();
     virtual int int_protection(int from, int to);
