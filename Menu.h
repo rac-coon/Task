ef0dacd77471c6a879df20808479fec5ba471af5
Nikita Solomientsev <38670044+araneoAnicet@users.noreply.github.com>
2020-08-28 19:05:26 +0300

    Add code review comments

--- Menu.h
+++ Menu.h
@@ -11,6 +11,7 @@ public:
     void show_menu();
     void menu_choice_main();
     int choose_student();
+    // EDIT: move the implementation into .cpp file. Constructors need the have the same name as their class name and don't require the return value type.
     void menu(){
         show_menu();
         menu_choice_main();
